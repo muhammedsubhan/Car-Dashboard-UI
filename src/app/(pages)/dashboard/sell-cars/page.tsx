22fff4e6f42e7dd6c752bac21b3acc0f7bb23fad
muhammedsubhan <muhammedsubhan692@gmail.com>
2024-03-24 09:58:20 +0500

    Tracking History Charts

--- src/app/(pages)/dashboard/sell-cars/page.tsx
+++ src/app/(pages)/dashboard/sell-cars/page.tsx
@@ -1,7 +1,43 @@
+import TrackingHistoryChart from "@/app/ChartsUI/TrackingHistoryChart";
+import Image from "next/image";
 import React from "react";
 
 const SellCars = () => {
-  return <div>SellCars</div>;
+  return (
+    <>
+      <div className="bg-slate-100 py-5 px-8">
+        <div>
+          <h1 className="text-2xl font-semibold">Sell Cars</h1>
+        </div>
+        <div className="py-10 flex justify-between">
+          <div className="bg-white h-[410px] w-[750px] flex flex-col justify-center gap-2 rounded-2xl">
+            <div className="px-8">
+              <h1 className="text-2xl font-semibold">2022 Mercedes Benz</h1>
+            </div>
+            <div className="flex items-center justify-center">
+              <Image
+                src="/car-frame.png"
+                alt="sell-cars-image"
+                width={550}
+                height={550}
+                priority
+              />
+            </div>
+          </div>
+          <div>
+            <div className="bg-white w-[460px] flex flex-col rounded-2xl">
+              <div className="px-8 mt-3 py-3">
+                <p className="text-black text-2xl font-semibold">
+                  Tracking History
+                </p>
+              </div>
+              <TrackingHistoryChart />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default SellCars;
---
+++ src/app/ChartsUI/TrackingHistoryChart.tsx
@@ -0,0 +1,73 @@
+"use client";
+import React from "react";
+import ReactApexCharts from "react-apexcharts";
+
+const TrackingHistoryChart = () => {
+  const options = {
+    series: [
+      {
+        data: [21, 22, 10, 28, 16, 21, 40],
+        name: "Km/h",
+      },
+    ],
+    chart: {
+      height: 328,
+      type: "bar",
+      toolbar: {
+        show: false, // Disable the toolbar controls
+      },
+    },
+    colors: ["#FF6370"], // Blue color
+
+    plotOptions: {
+      bar: {
+        columnWidth: "55%",
+
+        borderRadius: 5,
+        distributed: true,
+        dataLabels: {
+          position: "top", // top, center, bottom
+        },
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    legend: {
+      show: false,
+    },
+    xaxis: {
+      categories: [
+        ["Mon"],
+        ["Tue"],
+        ["Wed"],
+        ["Thu"],
+        ["Fri"],
+        ["Sat"],
+        ["Sun"],
+      ],
+      labels: {
+        style: {
+          colors: "gray", // Blue color
+          fontSize: "14px",
+        },
+      },
+    },
+    yaxis: {
+      show: true, // Disable y-axis data
+    },
+  };
+
+  return (
+    <div className="w-[450px]">
+      <ReactApexCharts
+        options={options}
+        series={options.series}
+        type="bar"
+        height={328}
+      />
+    </div>
+  );
+};
+
+export default TrackingHistoryChart;
