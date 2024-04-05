dcbe751d5f2fb6cc2c460470b63628af52aa99c9
muhammedsubhan <muhammedsubhan692@gmail.com>
2024-04-05 05:13:19 +0500

    bugs

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,39 +1,39 @@
-// import NextAuth from "next-auth";
-// import authconfig from "./auth.config";
-// import {
-//   DEFAULT_LOGIN_RDIRECT,
-//   apiAuthPrefix,
-//   authRoutes,
-//   publicRoutes,
-// } from "../routes";
+import NextAuth from "next-auth";
+import authconfig from "./auth.config";
+import {
+  DEFAULT_LOGIN_RDIRECT,
+  apiAuthPrefix,
+  authRoutes,
+  publicRoutes,
+} from "../routes";
 
-// const { auth } = NextAuth(authconfig);
+const { auth } = NextAuth(authconfig);
 
-// export default auth((req) => {
-//   // const { nextUrl } = req;
-//   // const isLoggedIn = !!req.auth;
-//   // const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
-//   // const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
-//   // const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+export default auth((req: any) => {
+  // const { nextUrl } = req;
+  // const isLoggedIn = !!req.auth;
+  // const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  // const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  // const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
-//   // if (isApiAuthRoute) {
-//   //   return null;
-//   // }
+  // if (isApiAuthRoute) {
+  //   return null;
+  // }
 
-//   // if (isAuthRoute) {
-//   //   if (isLoggedIn) {
-//   //     return Response.redirect(new URL(DEFAULT_LOGIN_RDIRECT, nextUrl));
-//   //   }
-//   //   return null;
-//   // }
+  // if (isAuthRoute) {
+  //   if (isLoggedIn) {
+  //     return Response.redirect(new URL(DEFAULT_LOGIN_RDIRECT, nextUrl));
+  //   }
+  //   return null;
+  // }
 
-//   // if (!isLoggedIn && !isPublicRoute) {
-//   //   return Response.redirect(new URL("/signin", nextUrl));
-//   // }
+  // if (!isLoggedIn && !isPublicRoute) {
+  //   return Response.redirect(new URL("/signin", nextUrl));
+  // }
 
-//   return null;
-// });
+  return null;
+});
 
-// export const config = {
-//   matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-// };
+export const config = {
+  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
